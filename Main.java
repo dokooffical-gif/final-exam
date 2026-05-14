6da71a27b79a60a182242d3a11d2fae98ebd0a26
dokooffical-gif <dokooffical@gmail.com>
2026-05-14 12:51:01 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(5, 7);
+        System.out.println("Result: " + result);
+    }
+}
